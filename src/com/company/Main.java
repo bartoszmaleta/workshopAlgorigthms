4433ff8137a1d771a3f1e8e8b87d2e03d1162e93
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-09 11:46:07 +0200

    Add secondTree

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("first tree -------------------");
         first_tree(5);
+
+        System.out.println("second tree -------------------");
+        second_tree(6);
     }
 
     public static void first_tree(int size) {
@@ -12,6 +15,18 @@ public class Main {
         for (int i = 1; i <= size; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print('*');
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void second_tree(int height) {
+        for (int i = 1; i < height + 1; i++) {
+            for (int j = 0; j < height - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i -1); k++) {
+                System.out.print("*");
             }
             System.out.print("\n");
         }
